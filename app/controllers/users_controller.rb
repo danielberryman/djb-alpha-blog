bc1954bea3aee2992cbbf5445375b425cc66b34b
Daniel Berryman <danielberryman90@gmail.com>
2018-04-21 11:20:22 -0400

    added edit user functionality and UI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:update]
+
+	def index
+
+	end
+
 	def new
 		@user = User.new
 	end
 
 	def create
-		@user = User.create(user_params)
+		@user = User.new(user_params)
 		if @user.save
 			flash[:success] = "Welcome to the alpha blog #{@user.username}"
 			redirect_to articles_path
@@ -13,7 +19,24 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		if @user.update(user_params)
+			flash[:success] = "User account updated successfully!"
+			redirect_to articles_path
+		else
+			render 'edit'
+		end
+	end
+
 	private
+
+	def set_user
+		@user = User.find(params[:id])
+	end
 
 	def user_params
 		params.require(:user).permit(:username, :email, :password)
