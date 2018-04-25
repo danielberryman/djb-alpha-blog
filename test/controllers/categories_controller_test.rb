62a9d648ecc2827698c5a043a4974f78efe66ef8
Daniel Berryman <danielberryman90@gmail.com>
2018-04-25 15:47:22 -0400

    added test for redirect on create action when admin is not logged in

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class CategoriesControllerTest < ActionController::TestCase
   def setup 
   	@category = Category.create(name: 'sports')
+  	@user = User.create(username: "john", email: "john@mail.com", password: "password", admin: true)
   end
 
   test 'should get categories index' do
@@ -11,6 +12,7 @@ class CategoriesControllerTest < ActionController::TestCase
   end
 
   test 'should get new' do
+  	session[:user_id] = @user.id
   	get :new
   	assert_response :success
   end
@@ -19,4 +21,16 @@ class CategoriesControllerTest < ActionController::TestCase
   	get(:show, {'id' => @category.id})
   	assert_response :success
   end	
+
+  test 'should redirect create when admin not logged in' do
+  	#we haven't logged an admin in in our above tests
+  	assert_no_difference 'Category.count' do
+  	#this means that category.count will not change will be true
+  	#to test is any post request for a new item is made use the .count method
+  	  post :create, category: { name: 'sports'}
+  	  #this is what happens (when this happens does category count change return to be true?)
+  	end
+  	assert_redirected_to categories_path
+  end
+
 end
